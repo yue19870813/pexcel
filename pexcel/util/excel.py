4c22916b6a6ec08b41cdd811780ae88474da4b14
ituuz <yue19870813@163.com>
2018-04-24 19:27:01 +0800

    提交excel工具类接口定义

---
+++ pexcel/util/excel.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Filename: excel.py
+
+' excel module '
+
+__author__ = 'ituuz'
+
+"""
+本模块是完全处理excel所需要的所有接口
+Excel 格式如下：
+配表名字	template
+描述		配表测试模版	
+其他		作者:ituuz  日期：2018-04-24	
+id			数值		说明
+int			string		string
+int			string	
+id			value		des
+KEY		
+10000		test1		测试1
+21001		test2		测试2
+22001		test3		测试3
+
+NOTE:
+模版文件参考template目录下的t_template.xlsx文件
+"""
+
+
+"""
+将excel转换为数据列表
+return：表名，配表描述，配表其他信息，数据
+
+数据格式：[
+			[属性名1, 属性名2，属性名3],
+			[数据类型1,数据类型2, 数据类型3],
+			[数据1, 数据2, 数据3],
+			...
+			...
+			...
+			[数据1, 数据2, 数据3]
+		  ]
+"""
+def convertExcel2List(filename):
+	pass
+
+
