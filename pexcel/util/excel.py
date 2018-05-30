a256a14c088d7f6ae196e29d180fe22568f3d797
sunyingying <ybb8131225@163.com>
2018-05-30 18:37:40 +0800

    excel analysis

--- pexcel/util/excel.py
+++ pexcel/util/excel.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Filename: excel.py
+import openpyxl
+import os
 
 ' excel module '
 
@@ -41,7 +43,45 @@ return：表名，配表描述，配表其他信息，数据
 			[数据1, 数据2, 数据3]
 		  ]
 """
-def convertExcel2List(filename):
-	pass
+
+def convertExcel2List(filename, sheetname):
+	data = openpyxl.load_workbook(filename)
+	if(sheetname == ""):
+		sheet = data.get_active_sheet()
+	else:
+		sheet = data.get_sheet_by_name(sheetname)
+
+	excelName = sheet.cell(row=1, column=2).value
+	excelDes = sheet.cell(row=2, column=2).value
+	excelOther = sheet.cell(row=3, column=2).value
+
+	print("表名：", excelName)
+	print("配表描述：", excelDes)
+	print("配表其他信息：", excelOther)
+
+	print("属性名：", end="")
+	for rowProperty in sheet.iter_rows(min_row=4, max_col=3, max_row=4):
+		for cell in rowProperty:
+			print(cell.value, "\t", end=",")
+	print()
+
+	print("数据类型：", end="")
+	for rowNumClass in sheet.iter_rows(min_row=6, max_col=3, max_row=6):
+		for cell in rowNumClass:
+			print(cell.value, "\t", end=",")
+	print()
+
+	print("数据：")
+	for row in sheet.iter_rows(min_row=9, max_col=3):
+		for cell in row:
+			print(cell.value, "\t", end=",")
+		print()
+	
+
+def main():
+	convertExcel2List("../../template/t_template.xlsx", "")
+
+if __name__=="__main__":
+	main()
 
 
