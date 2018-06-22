b8d79d54f613ecf59234e7104652be1ae2868609
yue19870813 <yue19870813@163.com>
2018-06-22 15:45:26 +0800

    add text data format

--- pexcel/generator/generator.py
+++ pexcel/generator/generator.py
@@ -20,11 +20,11 @@ __author__ = 'ituuz'
 """
 def generate (tableDataDict):
 	if config.OUTPUT_DATA_TYPE == config.OutputDataType.Json:
-		toJson(tableDataDict)
+		jsonStr = toJson(tableDataDict)
 	elif config.OUTPUT_DATA_TYPE == config.OutputDataType.Text:
-		pass
+		textStr = toText(tableDataDict)
 	elif config.OUTPUT_DATA_TYPE == config.OutputDataType.Binary:
-		pass
+		toBinary(tableDataDict)
 	else:
 		# 不存在这种生成数据的格式
 		print (language.DATA_GENERATE_TYPE_NOT_EXIST + "：" + config.OUTPUT_DATA_TYPE)
@@ -71,11 +71,18 @@ def toJson(tableDataDict):
 	return "json"
 
 # 将数据转换成纯文本格式数据
-def toText(self):
-	pass
+def toText(tableDataDict):
+	"""
+	int,string,string
+	id,xx,yy
+	10000,10001_200,10002_1
+	10001,10004_1000,BBB
+	10002,10003_444,ccc
+	"""
+	return "text"
 
 # 将数据转换为二进制格式数据
-def toBinary(self):
+def toBinary(tableDataDict):
 	pass
 
 
